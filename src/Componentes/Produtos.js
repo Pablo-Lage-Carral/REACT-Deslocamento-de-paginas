f8c428f1f4f6ece099bce17e527a42d3cb9fea3b
Pablo Lage Carral <124844088+Pablo-Lage-Carral@users.noreply.github.com>
2023-03-10 23:17:50 -0300

    Create Produtos.js

---
+++ src/Componentes/Produtos.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Produtos(){
+    const produtos =  {
+        backgroundColor: '#006400',
+        height:'85vH',
+        textAlign:'center',
+        color:'white'
+    }
+
+    return(
+        <>
+            <div style={produtos}>
+            <h1>Página principal Produtos</h1>
+            <p>Exemplo de página de produtos</p>
+            </div>
+        </>
+    )
+}
