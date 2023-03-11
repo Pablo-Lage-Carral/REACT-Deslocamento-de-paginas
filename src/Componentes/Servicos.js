f575654f3850572d1a969de5706a0839f1a33dd6
Pablo Lage Carral <124844088+Pablo-Lage-Carral@users.noreply.github.com>
2023-03-10 23:18:10 -0300

    Create Servicos.js

---
+++ src/Componentes/Servicos.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Servicos(){
+    const servicos =  {
+        backgroundColor: '#00FFFF',
+        height:'85vH',
+        textAlign:'center',
+        color:'orange'
+    }
+    return(
+        <>
+            <div style={servicos}>
+            <h1>Página de serviços</h1>
+            <p>Exemplo de página dos serviços.</p>
+            </div>
+        </>
+    )
+
+}
